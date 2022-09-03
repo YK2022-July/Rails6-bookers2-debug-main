1e51c666eb4ad8052f14477fb32ddc970e0815d5
YK2022-July <105860045+YK2022-July@users.noreply.github.com>
2022-09-03 14:12:00 +0000

    [Finish]kadai_3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,9 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    #@user = User.find(params[:id])
     @books = Book.new
     @users = current_user
+    @book_comment = BookComment.new
   end
 
   def index
