0480b8cb314058b92db47ce8af87362136a30ce6
YK2022-July <105860045+YK2022-July@users.noreply.github.com>
2022-09-06 10:57:39 +0000

    [Finish]kadai_5

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,7 +5,7 @@ class SearchesController < ApplicationController
     @search_model = params[:search_model]
     @word = params[:word]
 
-    if @search_model == "User"
+    if @search_model == "user"
       @users = User.looks(params[:search_pattern], params[:word])
     else
       @books = Book.looks(params[:search_pattern], params[:word])
